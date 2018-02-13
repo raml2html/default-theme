3e4466ae381299da47167253b5cad5b0972f279f
Trevor Robinson <trevor@scurrilous.com>
2018-02-13 09:37:37 -0600

    Enable pretty option in render-all-examples for easier diffing (#16)

--- examples/render-all-examples.js
+++ examples/render-all-examples.js
@@ -5,11 +5,12 @@ const fs = require('fs');
 process.chdir(__dirname);
 
 const config = raml2html.getConfigForTheme('raml2html-default-theme');
+const options = { pretty: true };
 const examples = glob.sync('*.raml');
 
 examples.forEach((ramlFile) => {
   console.log(ramlFile);
-  raml2html.render(ramlFile, config).then((result) => {
+  raml2html.render(ramlFile, config, options).then((result) => {
     const filename = ramlFile.replace('.raml', '.html');
     fs.writeFileSync(filename, result);
   }, (error) => {
